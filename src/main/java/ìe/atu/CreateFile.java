2f3df2967c2320c134313ee33dbb5c46e0ff2dd9
G00403931 <G00403931@atu.ie>
2022-11-15 14:06:45 +0000

    added fileWriter in CreateFile

--- src/main/java/ìe/atu/CreateFile.java
+++ src/main/java/ìe/atu/CreateFile.java
@@ -1,4 +1,23 @@
 package ìe.atu;
 
+import java.io.*;
+
 public class CreateFile {
+    public static void main(String[] args){
+
+        {
+            File myFile = new File("MyFile.txt");
+            System.out.println("My file is locate at " + myFile.getAbsolutePath());
+            String content = "Details to write";
+            try
+            {
+                FileWriter myWriter = new FileWriter(myFile, true);
+                myWriter.write("pog champ");
+                myWriter.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
